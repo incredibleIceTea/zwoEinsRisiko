6f8b8d4b048226e5f0bf7f2f4f1b088bbf28bb56
incredibleIceTea <incredible.ice.tea@gmail.com>
2019-11-01 23:11:51 +0100

    comments

--- Module/ipExtract.ps1
+++ Module/ipExtract.ps1
@@ -1,3 +1,12 @@
+<#
+
+    Modul zum auslesen statischer IP-Konfigurationen aller Netzwerk-Adapter
+
+    Autor: incredibleIceTea
+    Version: 0.1a
+
+#>
+
 $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 $filePath =  Join-Path -Path $desktopPath -ChildPath "ipconfig.txt"
 $iptable = Get-NetIPAddress
@@ -8,10 +17,12 @@ $header = "MACADDRESS SPECIAL INTERFACENAME IP"
 $ret += $header
 foreach($ip in $iptable){
     if($null -ne $ip.IPv4Address){
+        # Beschränkung auf statische Konfiguration
         if($ip.suffixOrigin -eq "manual"){
             $name = $ip.InterfaceAlias
             $suffix = $ip.suffixOrigin
             $address = $ip.IPv4Address
+            # MacAdesse zum dem Interface zuordnen
             foreach($a in $adapters){
                 if($a.name = $name){
                     $mac = $a.macAddress
